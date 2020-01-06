6c0f53521f215b29202d26ac80aaf84d3ffd8966
Quintin Kovach <datafrogman@outlook.com>
2020-01-06 02:34:04 -0500

    Tested code and created a sample implementation

--- challenges.py
+++ challenges.py
@@ -1,3 +1,6 @@
+import requests
+import paramiko
+
 class Challenge(object):
 
     def __init__(self, host, name):
@@ -21,6 +24,31 @@ class Challenge(object):
 
         raise NotImplementedError("{} has not implemented the 'redeployment' function".format(self.__class__.__name__))
 
-"""
-Place Challenge classes here
+""" Sample implementation for a challenge that is just a webpage with the flag on it
+class sample(Challenge):
+    def __init__(self, host, name, keyfile):
+        self.host = host
+        self.name = name
+        self.flag = "RITSEC{sample}"
+        self.port = 8080
+        self.key = keyfile
+
+    def solve(self):
+        try:
+            response = requests.get('http://{}:{}'.format(self.host, self.port))
+        except:
+            return False
+        if self.flag in response.text:
+            return True
+        else:
+            return False
+
+    def redeployment(self):
+        k = paramiko.RSAKey.from_private_key_file(self.key)
+        ssh = paramiko.SSHClient()
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(self.host, port=22, username="datafrogman", pkey=k)
+        stdin, stdout, stderr = ssh.exec_command("cd sample; sudo docker-compose down; sudo docker-compose up -d")
+        ssh.close()
 """
--- downDetector.py
+++ downDetector.py
@@ -14,30 +14,47 @@ class InvalidChallengeError(Error):
 
 #Function to check if a challenge is up using the solve function and redeploy if it is not
 def check(challenge):
-    logging.info("Checking challenge {}".format(challenge.__class__.__name__))
+    log.debug("Checking challenge {}".format(challenge.__class__.__name__))
     if challenge.solve():
-        logging.info("{} is up".format(challenge.__class__.__name__))
+        log.debug("{} is up".format(challenge.__class__.__name__))
     else:
-        logging.info("{} is down".format(challenge.__class__.__name__))
-        logging.info("Redeploying {}".format(challenge.__class__.__name__))
+        log.debug("{} is down".format(challenge.__class__.__name__))
+        log.debug("Redeploying {}".format(challenge.__class__.__name__))
         challenge.redeployment()
-        logging.info("Redeployed {}".format(challenge.__class__.__name__))
+        log.info("Redeployed {}".format(challenge.__class__.__name__))
 
 if __name__ == "__main__":
+
+    #Set up the parser
     parser = argparse.ArgumentParser(description="Automatically verify and redeploy implemented challenges")
     parser.add_argument("-l", "--list", action="store_true", help="List the challenges currently implemented")
     parser.add_argument("-t", "--target", nargs='+', help="Target challenges to test")
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("-f", "--file", help="Log file", default="./downDetector.log") 
     args = parser.parse_args()
-
+    
+    # Example implementation of the 'sample' challenge
+    #sample = sample('192.168.76.128', 'sample', 'test.pem')
+    #challengesList = [sample]
     challengesList = []
+    
+    #Set up the logger
     logfile = args.file
-    logging.basicConfig(filename=logfile, filemode="w", level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s: %(message)s", 
+                              datefmt="%Y-%m-%d - %H:%M:%S")
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+    
+    #Begin handling the args
     if args.verbose:
-        console = logging.StreamHandler()
-        console.setLevel(logging.INFO)
-        logging.getLogger("").addHandler(console)
+        log.addHandler(ch)
     if args.list:
         temp = "Challenges Implemented: "
         if len(challengesList) == 0:
@@ -49,7 +66,7 @@ if __name__ == "__main__":
     elif args.target is not None:
         for option in args.target:
             if len(challengesList) == 0:
-                logging.info("No challenges")
+                log.debug("No challenges")
                 sys.exit()
             for i in challengesList:
                 if i.__class__.__name__ == option:
@@ -58,7 +75,7 @@ if __name__ == "__main__":
                     raise InvalidChallengeError("{} is not a valid challenge".format(option))
     else:
         if len(challengesList) == 0:
-            logging.info("No challenges")
+            log.debug("No challenges")
             sys.exit()
         for entry in challengesList:
             check(entry)
