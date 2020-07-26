758f9149704e938ff514341c15b7866457bb9976
Quintin Walters <DataFrogman@outlook.com>
2020-07-25 21:33:51 -0700

    initial refactoring for web display

--- challenges.py
+++ challenges.py
@@ -1,15 +1,21 @@
 import requests
 import paramiko
+from datetime import datetime
 
 class Challenge(object):
 
     def __init__(self, host, name):
         self.host = host
         self.name = name
+        self.status = False
+        self.latestUpTime = "0000-00-00 - 00:00:00" 
 
     def solve(self):
         """This is the solver to check if a challenge is up or vandalized.
         IT MUST BE DEFINED.
+        
+        For the web frontend to work properly this must set self.status to the result of the check before returning.
+        If the check succeeds you must set the self.latestUpTime to the timestamp, format is "YYYY-MM-DD - HH:MM:SS"
 
         Return True if the challenge passes all checks, return False at the first failure
         """
@@ -37,10 +43,15 @@ class sample(Challenge):
         try:
             response = requests.get('http://{}:{}'.format(self.host, self.port), timeout=2)
         except:
+            self.status = False
             return False
         if self.flag in response.text:
+            self.status = True
+            now = datetime.now()
+            self.latestUpTime = now.strftime("%Y-%m-%d - %H:%M:%S")
             return True
         else:
+            self.status = False
             return False
 
     def redeployment(self):
